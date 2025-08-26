1648a14c00959103fdc9fdc10e887e4112a52284
Sofia Ventura <128555651+sofiavxntura@users.noreply.github.com>
2025-08-26 13:09:01 -0500

    Update ex1.c

--- Slide4/ex1.c
+++ Slide4/ex1.c
@@ -1,1 +1,28 @@
+#include <stdio.h>
 
+int eh_primo(int k){
+    if (k<=1) {
+        return 0;
+    }
+    for (int i = 2; i < k; i++) {
+        if ((k%i) == 0){
+            return 0;
+        }
+    }
+    return 1; //não conseguiu dividir com nada.
+}
+
+int main(){
+    int num;
+    printf("Insira um número: ");
+    scanf("%d", &num);
+
+    int resultado = eh_primo(num);
+    if (resultado == 1){
+        printf("%d é primo.\n",num);
+    }
+    else {
+        printf("%d não é primo.\n",num);
+    }
+    return 0;
+}
