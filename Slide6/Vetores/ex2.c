4eeb2db93e1ff248530fa5c590b4d366fafff1ef
Sofia Ventura <128555651+sofiavxntura@users.noreply.github.com>
2025-09-09 12:57:57 -0500

    Update ex2.c

--- Slide6/Vetores/ex2.c
+++ Slide6/Vetores/ex2.c
@@ -1,1 +1,37 @@
+#include <stdio.h>
 
+int main(){
+    int vet[5]; int num = 0;
+    int I = 0; // entre 0 e 4, inclusive
+
+    for (int i = 0; i < 5; i++) {
+        printf("Insira um número: ");
+        scanf("%d", &num);
+        vet[i] = num;
+    }
+
+    for (int i = 0; i < 5; i++) {
+        printf("| %d |",vet[i]);
+    }
+    printf("\n---------\n");
+
+    printf("Insira o valor de I (0 a 4): ");
+    scanf("%d",&I);
+    vet[I] = 0;
+    
+    for (int k = 0; k < 4; k++) { // nao quero 0, nem 4
+        if (vet[k] == 0) {
+            vet[k] = vet[k+1];
+            vet[k+1] = 0;
+        }
+
+    }
+
+    printf("---------\n");
+
+    for (int i = 0; i < 5; i++) {
+        printf("| %d |",vet[i]);
+    }
+    printf("\n");
+
+}
