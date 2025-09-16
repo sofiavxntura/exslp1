9c628149140874ab061dcecca46f82a9fcf48fbf
Sofia Ventura <128555651+sofiavxntura@users.noreply.github.com>
2025-09-16 14:52:21 -0300

    Update ex1.c

--- Slide6/Strings/ex1.c
+++ Slide6/Strings/ex1.c
@@ -1,1 +1,26 @@
 
+#include <stdio.h>
+#include <string.h>
+
+int tamanho(char *str){
+    int count = 0;
+
+    while (str[count] != '\0') { //operador nulo
+        count++;
+    }
+    return count;
+}
+
+
+
+int main(){
+    char var[100];
+
+    printf("Insira sua string: ");
+    scanf("%99s",var); //limitar a 99
+    int size = tamanho(var);
+    printf("Tamanho da string '%s': %d\n",var,size);
+
+
+    return 0;
+}
