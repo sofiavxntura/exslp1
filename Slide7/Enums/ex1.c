edee53c6ba38163bab5241b6b8c4db8aa135047f
Sofia Ventura <128555651+sofiavxntura@users.noreply.github.com>
2025-09-26 18:47:51 -0300

    Update ex1.c

--- Slide7/Enums/ex1.c
+++ Slide7/Enums/ex1.c
@@ -1,1 +1,21 @@
+#include <stdio.h>
 
+enum Dia {
+    Seg, // 0 
+    Ter, // 1...
+    Qua,
+    Qui,
+    Sex,
+    Sab,
+    Dom,
+};
+
+char* nome(enum Dia x){
+    char* semana[7] = {"Segunda","Terça","Quarta","Quinta","Sexta","Sábado","Domingo"};
+    return semana[x];
+};
+
+int main(){
+    printf("%s\n",nome(Qui));
+
+}
