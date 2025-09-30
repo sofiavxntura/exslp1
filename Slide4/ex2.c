4aed8ffbb4a33094c988796fa13fa363c64f8d32
Sofia Ventura <128555651+sofiavxntura@users.noreply.github.com>
2025-09-30 06:01:07 -0500

    Atualizar o ex2.c

--- Slide4/ex2.c
+++ Slide4/ex2.c
@@ -1,27 +1,36 @@
-#include <stdio.h>
-
-int todos_os_primos(int k){
-    if (k<=1) {
+int eh_primo(int p) {
+    if (p <= 1) {
         return 0;
     }
-    for (int i = 2; i < k; i++) {
-        if ((k%i) == 0){
+    for (int i = 2; i < p; i++) {
+        if ((p % i) == 0) {
             return 0;
         }
     }
+    return 1; // não conseguiu dividir com nada
+}
+
+int todos_os_primos(int k) {
+    if (k <= 1) {
+        return 0;
+    }
+    
+    printf("Números primos até %d: ", k);
+    for (int i = 2; i <= k; i++) {
+        if (eh_primo(i)) {
+            printf("%d ", i);
+        }
+    }
+    printf("\n");
     return 1;
 }
 
-int main(){
+int main() {
     int num;
     printf("Insira um número: ");
     scanf("%d", &num);
 
-    for (int g = 1; g < num;g++) {
-        if (todos_os_primos(g)){
-            printf("%d, ",g);
-        }
-    }
-    printf("\n");
+    todos_os_primos(num);
+
     return 0;
 }
