4e990b603417425623317c2a3906e549aa04e0b7
Sofia Ventura <128555651+sofiavxntura@users.noreply.github.com>
2025-10-11 23:35:08 -0300

    Update ex1.c

--- Slide8/ex1.c
+++ Slide8/ex1.c
@@ -1,1 +1,20 @@
+#include <stdio.h>
+#include <string.h>
 
+int main(){
+    char texto[26];
+    FILE *arq1 = fopen("arquivo1.txt","w"); //w = write
+
+    printf("Insira uma string (máx 25 caracteres): ");
+    scanf("%25s",texto);
+
+    int tamanho = strlen(texto);
+    for (int i = 0; i < tamanho; i++){
+        fputc(texto[i],arq1);
+    }
+    fclose(arq1);
+
+    printf("Tamanho da string: %d caracteres",tamanho);
+    
+    return 0;
+}
