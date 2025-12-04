ff5f5181e3247339eca9dbeb19bb9c7d4056a565
Sofia Ventura <128555651+sofiavxntura@users.noreply.github.com>
2025-12-04 14:27:14 -0300

    Update ex1.c

--- Slide10/ex1.c
+++ Slide10/ex1.c
@@ -1,1 +1,32 @@
+#include <stdio.h>
 
+struct Caixa {
+    int valor;
+    struct Caixa* prox;
+};
+
+int main(){
+    struct Caixa c1; c1.valor = 1;
+    struct Caixa c2; c2.valor = 3;
+    struct Caixa c3; c3.valor = 9;
+    struct Caixa c4; c4.valor = 7;
+    struct Caixa c5; c5.valor = 5;
+
+    c1.prox = &c2;
+    c2.prox = &c3;
+    c3.prox = &c4;
+    c4.prox = &c5;
+    c5.prox = NULL;
+
+    struct Caixa* atual = &c1;
+
+    while (atual != NULL) {
+        printf("%d", atual->valor);
+        atual = atual->prox;
+        if (atual != NULL) { //ainda tem numero a seguir
+            printf(" -> ");
+        }
+    }
+    printf("\n");
+    return 0;
+}
