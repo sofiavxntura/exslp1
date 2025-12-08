d46b1e322c50dee4dc21f72c446dff463042874d
Sofia Ventura <128555651+sofiavxntura@users.noreply.github.com>
2025-12-08 20:25:56 -0300

    Update ex1.c

--- Slide10/ex1.c
+++ Slide10/ex1.c
@@ -1,32 +1,42 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 struct Caixa {
     int valor;
     struct Caixa* prox;
 };
 
-int main(){
-    struct Caixa c1; c1.valor = 1;
-    struct Caixa c2; c2.valor = 3;
-    struct Caixa c3; c3.valor = 9;
-    struct Caixa c4; c4.valor = 7;
-    struct Caixa c5; c5.valor = 5;
-
-    c1.prox = &c2;
-    c2.prox = &c3;
-    c3.prox = &c4;
-    c4.prox = &c5;
-    c5.prox = NULL;
-
-    struct Caixa* atual = &c1;
-
+int main() {
+    struct Caixa* c1 = (struct Caixa*)malloc(sizeof(struct Caixa));
+    struct Caixa* c2 = (struct Caixa*)malloc(sizeof(struct Caixa));
+    struct Caixa* c3 = (struct Caixa*)malloc(sizeof(struct Caixa));
+    struct Caixa* c4 = (struct Caixa*)malloc(sizeof(struct Caixa));
+    struct Caixa* c5 = (struct Caixa*)malloc(sizeof(struct Caixa));
+    
+    
+    c1->valor = 1;
+    c2->valor = 3;
+    c3->valor = 9;
+    c4->valor = 7;
+    c5->valor = 5;
+    
+    c1->prox = c2;
+    c2->prox = c3;
+    c3->prox = c4;
+    c4->prox = c5;
+    c5->prox = NULL;  // fim. se fosse circular apontava pra c1
+    
+    printf("Lista encadeada: ");
+    struct Caixa* atual = c1;
     while (atual != NULL) {
         printf("%d", atual->valor);
-        atual = atual->prox;
-        if (atual != NULL) { //ainda tem numero a seguir
+        if (atual->prox != NULL) {
             printf(" -> ");
         }
+        atual = atual->prox;
     }
     printf("\n");
+    free(c1); free(c2); free(c3); free(c4); free(c5);
+    
     return 0;
 }
